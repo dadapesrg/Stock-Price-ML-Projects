f3adc384dd5a1dd8e21ac7f0698e4dea14206a4b
update-branch <josephodada@gmail.com>
2024-12-09 19:34:57 +0000

    Add Convolution Neural Network (CNN) model

--- model/cnn_tensorFlow_model.py
+++ model/cnn_tensorFlow_model.py
@@ -6,7 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
+from keras.layers import Dense, Flatten
+from keras.layers import Conv1D, MaxPooling1D
+import keras.optimizers
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from data_preprocessor import read_database_data
@@ -52,33 +54,53 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Initialize the model
 model = Sequential()
 
-# Add LSTM layers with Dropout regularization
-model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=False))
-model.add(Dropout(0.2))
-# Add Dense layer
-model.add(Dense(units=25))
-model.add(Dense(units=1))  # Output layer, predicting the 'close' price
+lr = 0.0003
+adam = keras.optimizers.Adam(lr)
 
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+#Develope convolusion CNN model for Time Series Forecasting
+
+# Define the CNN model
+model_cnn = Sequential()
+model_cnn.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
+model_cnn.add(MaxPooling1D(pool_size=2))
+model_cnn.add(Flatten())
+model_cnn.add(Dense(50, activation='relu'))
+model_cnn.add(Dense(1))
+model_cnn.compile(loss='mse', optimizer=adam)
+
+#CNN model summary
+model_cnn.summary()
+
+#Number of epoch and batch size
+epochs = 150
+batch = 24
+
 # Train the model
-model.fit(X_train, y_train, batch_size=8, epochs=100) #32
+cnn_history = model_cnn.fit(X_train, y_train, epochs=epochs, batch_size=batch, verbose=2)
 
-# Make predictions
-predictions = model.predict(X_test)
-predictions = scaler.inverse_transform(np.hstack((np.zeros((predictions.shape[0], 3)), predictions, np.zeros((predictions.shape[0], 1)))))[:, 3]
+# Make predictions on the test set
+cnn_y_pred = model_cnn.predict(X_test)
 
-# Inverse transform the test data for comparison
-y_test_scaled = scaler.inverse_transform(np.hstack((np.zeros((y_test.shape[0], 3)), y_test.reshape(-1, 1), np.zeros((y_test.shape[0], 1)))))[:, 3]
+# Make predictions on the test set
+cnn_y_pred = model_cnn.predict(X_test)
+
+# Since we scaled all features but predicting only one, we'll
+# need to inverse transform the predictions using the appropriate feature column.
+def invert_transform(data, shape, column_index, scaler):
+    dummy_array = np.zeros((len(data), shape))
+    dummy_array[:, column_index] = data.flatten()
+    return scaler.inverse_transform(dummy_array)[:, column_index]
+
+# Create a DataFrame to hold predictions and actual values
+test_pred_df = pd.DataFrame({
+    'Actual': invert_transform(y_test, X_train.shape[2], 0, scaler), 
+    'Predicted': invert_transform(cnn_y_pred, X_train.shape[2], 0, scaler)
+})
 
 # Plot the results
 plt.figure(figsize=(14,5))
-plt.plot(y_test_scaled, color='blue', label='Actual Stock Price')
-plt.plot(predictions, color='red', label='Predicted Stock Price')
+plt.plot(test_pred_df['Actual'], color='blue', label='Actual Stock Price')
+plt.plot(test_pred_df['Predicted'], color='red', label='Predicted Stock Price') #plt.plot(y_pred, color='red', label='Predicted Stock Price')
 plt.title('Stock Price Prediction')
 plt.xlabel('Time')
 plt.ylabel('Stock Price')
@@ -89,9 +111,10 @@ plt.show()
 #Import library for saving the model
 import pickle
 #Save the model to a file
-with open('results/lstm_stock_model.pkl', 'wb') as f:
+with open('results/cnn_stock_model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
 #calculate the r2_score
-R2_Score_dtr = round(r2_score(predictions, y_test_scaled) * 100, 2)
-print("R2 Score for LSTM : ", R2_Score_dtr,"%")
+#calculate the r2_score
+R2_Score_dtr = round(r2_score(cnn_y_pred, y_test) * 100, 2)
+print("R2 Score for CNN Model : ", R2_Score_dtr,"%")
